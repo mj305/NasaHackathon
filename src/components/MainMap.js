3793210bfdf66d7dc1e506d44c3169d32c336770
khaifahmi99 <khaifahmi99@github.com>
2020-10-04 19:09:07 +1100

    disable scroll zoom

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import * as parkData from "../data/skateboard-parks.json";
 
 function MainMap() {
+    const [activePoint, setActivePoint] = useState(null);
+
     return (
-        <Map center={[45.4, -75.7]} zoom={12}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
+      <div className="container">
+        <Map center={[37.9, -95.0]} zoom={4} scrollWheelZoom={false}>
+        <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+        {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={[
+            park.geometry.coordinates[1],
+            park.geometry.coordinates[0]
+          ]}
+        />
+      ))}
         </Map>
+      </div>
       );
 }
 
